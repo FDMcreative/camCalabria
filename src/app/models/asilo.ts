0cc122cbe5a709eaaf41e6ef893d3df1e7e7a898
FDMcreative-Ecubit <f.delmonte@ecubit.it>
2020-01-10 09:58:23 +0100

    working on asilo constructor

--- src/app/models/asilo.ts
+++ src/app/models/asilo.ts
@@ -10,8 +10,18 @@ export class Asilo {
 
 }
 
-    // idAsilo: number,
-    // nome: string,
-    // indirizzo: string,
-    // telefono: number,
-    // numeroStanze: number
+// export class Asilo {
+//     public idAsilo: string;
+//     public nome: string;
+//     public indirizzo: string;
+//     public telefono: number;
+//     public numeroStanze: number;
+
+//     constructor(idAsilo: string, nome: string, indirizzo: string, telefono: number, numeroStanze: number) {
+//         this.idAsilo = idAsilo;
+//         this.nome = nome;
+//         this.indirizzo = indirizzo;
+//         this.telefono = number;
+//         this.numeroStanze = number;
+//     }
+// }
